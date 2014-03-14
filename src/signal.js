fbb1b0bd7229fdabc0342139b16a02f76093e1f4
Chris Colbert <sccolbert@gmail.com>
2014-03-14 17:55:58 -0400

    add the start of a Signal type

---
+++ src/signal.js
@@ -0,0 +1,21 @@
+﻿/*-----------------------------------------------------------------------------
+| Copyright (c) 2014, Nucleic Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|
+| The full license is in the file COPYING.txt, distributed with this software.
+|----------------------------------------------------------------------------*/
+var porcelain;
+(function (porcelain) {
+    var Signal = (function () {
+        function Signal() {
+        }
+        Signal.prototype.connect = function (handler) {
+        };
+
+        Signal.prototype.disconnect = function (handler) {
+        };
+        return Signal;
+    })();
+})(porcelain || (porcelain = {}));
+//# sourceMappingURL=signal.js.map
---
+++ src/signal.ts
@@ -0,0 +1,17 @@
+﻿/*-----------------------------------------------------------------------------
+| Copyright (c) 2014, Nucleic Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|
+| The full license is in the file COPYING.txt, distributed with this software.
+|----------------------------------------------------------------------------*/
+module porcelain {
+
+    class Signal<Payload> {
+
+        connect(handler: (Payload) => void) { }
+
+        disconnect(handler: (Payload) => void) { }
+
+    }
+}
