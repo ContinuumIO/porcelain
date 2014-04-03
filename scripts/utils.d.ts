4400ca44fba725735c84fe387e7c7f62211e2439
Chris Colbert <sccolbert@gmail.com>
2014-04-03 12:46:18 -0400

    add a util function to get enum values

---
+++ scripts/utils.d.ts
@@ -0,0 +1,6 @@
+﻿declare module porcelain {
+    /**
+    * Create an array of values defined an enum object.
+    */
+    function enumValues<T extends number>(enumObj: any): T[];
+}
---
+++ scripts/utils.ts
@@ -0,0 +1,24 @@
+﻿/*-----------------------------------------------------------------------------
+| Copyright (c) 2014, Nucleic Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|
+| The full license is in the file COPYING.txt, distributed with this software.
+|----------------------------------------------------------------------------*/
+module porcelain {
+
+    /**
+     * Create an array of values defined an enum object.
+     */
+    export function enumValues<T extends number>(enumObj: any): T[] {
+        var values: T[] = [];
+        for (var k in enumObj) {
+            var v = enumObj[k];
+            if (typeof v === "number") {
+                values.push(v);
+            }
+        }
+        return values;
+    }
+
+}
