5c627796bacde45016df44189c50868ed4b04e51
Chris Colbert <sccolbert@gmail.com>
2014-04-14 13:32:26 -0400

    add an aggregate api module

---
+++ src/porcelain.ts
@@ -0,0 +1,28 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014, Nucleic Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|
+| The full license is in the file COPYING.txt, distributed with this software.
+|----------------------------------------------------------------------------*/
+
+/// <reference path="button.ts"/>
+/// <reference path="common.ts"/>
+/// <reference path="component.ts"/>
+/// <reference path="event_binder.ts"/>
+/// <reference path="label.ts"/>
+/// <reference path="layout_item.ts"/>
+/// <reference path="move_grip.ts"/>
+/// <reference path="point.ts"/>
+/// <reference path="push_button.ts"/>
+/// <reference path="rect.ts"/>
+/// <reference path="signal.ts"/>
+/// <reference path="size.ts"/>
+/// <reference path="size_grip.ts"/>
+/// <reference path="title_bar.ts"/>
+/// <reference path="utils.ts"/>
+/// <reference path="viewport.ts"/>
+/// <reference path="window.ts"/>
+/// <reference path="z_stack.ts"/>
+
+module porcelain { }
