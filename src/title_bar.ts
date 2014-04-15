9933feb833071d4a3e324d95c9ecf4fdff0a8983
Chris Colbert <sccolbert@gmail.com>
2014-04-15 17:27:26 -0400

    improve the API of TitleBar

--- src/title_bar.ts
+++ src/title_bar.ts
@@ -8,6 +8,20 @@
 module porcelain {
 
     /**
+     * An enum defining the available title bar buttons.
+     */
+    export
+    enum TitleBarButton {
+        NoButton = 0x0,
+        Close = 0x1,
+        Maximize = 0x2,
+        Minimize = 0x4,
+        Restore = 0x8,
+        Mask = 0xF,
+    }
+
+
+    /**
      * A simple title bar widget for use in a typical window.
      *
      * The title bar is a dumb container widget. The window is
@@ -67,91 +81,111 @@ module porcelain {
             super(target);
             this.addClass(TitleBar.Class);
 
-            var icon = new Component();
+            var icon = this._icon = new Component();
             icon.addClass(TitleBar.IconClass);
 
-            var label = new Label();
+            var label = this._label = new Label();
             label.addClass(TitleBar.LabelClass);
             label.addClass(CommonClass.LargeText);
 
-            var minBtn = new Button();
-            minBtn.addClass(TitleBar.MinimizeButtonClass);
-
-            var maxBtn = new Button();
-            maxBtn.addClass(TitleBar.MaximizeButtonClass);
-
-            var rstBtn = new Button();
-            rstBtn.addClass(TitleBar.RestoreButtonClass);
-
-            var clsBtn = new Button();
+            var clsBtn = this._closeButton = new Button();
             clsBtn.addClass(TitleBar.CloseButtonClass);
 
-            var btnBox = new Component();
+            var maxBtn = this._maximizeButton = new Button();
+            maxBtn.addClass(TitleBar.MaximizeButtonClass);
+
+            var minBtn = this._minimizeButton = new Button();
+            minBtn.addClass(TitleBar.MinimizeButtonClass);
+
+            var rstBtn = this._restoreButton = new Button();
+            rstBtn.addClass(TitleBar.RestoreButtonClass);
+
+            var btnBox = this._buttonBox = new Component();
             btnBox.addClass(TitleBar.ButtonBoxClass);
             btnBox.append(minBtn, rstBtn, maxBtn, clsBtn);
 
-            this._subItems = {
-                icon: icon,
-                label: label,
-                minimizeButton: minBtn,
-                maximizeButton: maxBtn,
-                restoreButton: rstBtn,
-                closeButton: clsBtn,
-                buttonBox: btnBox,
-            };
-
             // the order is important for CSS float layout
             this.append(icon, btnBox, label);
+
+            this.setButtons(TitleBarButton.Mask & ~TitleBarButton.Restore);
         }
 
         /**
          * Destroy the title bar.
          */
         destroy(): void {
-            this._subItems = null;
+            this._icon = null;
+            this._label = null;
+            this._minimizeButton = null;
+            this._maximizeButton = null;
+            this._restoreButton = null;
+            this._closeButton = null;
+            this._buttonBox = null;
             super.destroy();
         }
 
         /**
-         * Returns the icon item attached to the title bar.
+         * A signal emitted when the close button is clicked.
          */
-        icon(): Component {
-            return this._subItems.icon;
+        get closeButtonClicked(): Signal {
+            return this._closeButton.clicked;
         }
 
         /**
-         * Returns the label item attached to the title bar.
+         * A signal emitted when the maximize button is clicked.
          */
-        label(): Label {
-            return this._subItems.label;
+        get maximizeButtonClicked(): Signal {
+            return this._maximizeButton.clicked;
         }
 
         /**
-         * Returns the close button attached to the title bar.
+         * A signal emitted when the minimize button is clicked.
          */
-        closeButton(): Button {
-            return this._subItems.closeButton;
+        get minimizeButtonClicked(): Signal {
+            return this._minimizeButton.clicked;
         }
 
         /**
-         * Returns the restore button attached to the title bar.
+         * A signal emitted when the restore button is clicked.
          */
-        restoreButton(): Button {
-            return this._subItems.restoreButton;
+        get restoreButtonClicked(): Signal {
+            return this._restoreButton.clicked;
         }
 
         /**
-         * Returns the minimize button attached to the title bar.
+         * Returns the title text of the title bar.
          */
-        minimizeButton(): Button {
-            return this._subItems.minimizeButton;
+        title(): string {
+            return this._label.text();
         }
 
         /**
-         * Returns the maximize button attached to the title bar.
+         * Set the title text of the title bar.
          */
-        maximizeButton(): Button {
-            return this._subItems.maximizeButton;
+        setTitle(title: string): void {
+            this._label.setText(title);
+        }
+
+        /**
+         * Returns an OR'd combination of visible TitleBarButtons.
+         */
+        buttons(): number {
+            return this._buttons;
+        }
+
+        /**
+         * Set the OR'd combination of visible TitleBarButtons.
+         */
+        setButtons(buttons: number): void {
+            this._buttons = buttons & TitleBarButton.Mask;
+            this._closeButton.setDisplay(
+                buttons & TitleBarButton.Close ? "" : "none");
+            this._maximizeButton.setDisplay(
+                buttons & TitleBarButton.Maximize ? "" : "none");
+            this._minimizeButton.setDisplay(
+                buttons & TitleBarButton.Minimize ? "" : "none");
+            this._restoreButton.setDisplay(
+                buttons & TitleBarButton.Restore ? "" : "none");
         }
 
         /**
@@ -166,7 +200,7 @@ module porcelain {
             if (event.button !== 0) {
                 return;
             }
-            var elem = this._subItems.buttonBox.element();
+            var elem = this._buttonBox.element();
             var rect = new Rect(elem.getBoundingClientRect());
             var point = new Point(event.clientX, event.clientY);
             if (rect.contains(point)) {
@@ -175,21 +209,14 @@ module porcelain {
             super.onMouseDown(event);
         }
 
-        private _subItems: ITitleBarSubItems;
-    }
-
-
-    /**
-     * An interface for storing the sub items of a title bar.
-     */
-    interface ITitleBarSubItems {
-        icon: Component;
-        label: Label;
-        minimizeButton: Button;
-        maximizeButton: Button;
-        restoreButton: Button;
-        closeButton: Button;
-        buttonBox: Component;
+        private _icon: Component;
+        private _label: Label;
+        private _minimizeButton: Button;
+        private _maximizeButton: Button;
+        private _restoreButton: Button;
+        private _closeButton: Button;
+        private _buttonBox: Component;
+        private _buttons = TitleBarButton.NoButton;
     }
 
 }
