d9d52a0348f7b61ffc9fa89e11086fca18c6ad54
Chris Colbert <sccolbert@gmail.com>
2014-04-17 17:58:57 -0400

    start moving Component to extend Node

--- src/component.ts
+++ src/component.ts
@@ -10,10 +10,13 @@ module porcelain {
     /**
      * The most base class of porcelain objects.
      *
+     * The Component class supports DOM element manipulation. It
+     * will typically be subclassed to create concrete widgets.
+     *
      * @class
      */
     export
-    class Component {
+    class Component extends Node {
 
         /**
          * The CSS class added to Component instances.
@@ -21,116 +24,21 @@ module porcelain {
         static Class = "p-Component";
 
         /**
-         * A signal emitted when the component is destroyed.
-         *
-         * @readonly
-         */
-        destroyed = new Signal();
-
-        /**
          * Construct a new Component.
          */
         constructor() {
+            super();
             this._element = this.createElement();
             this.addClass(Component.Class);
         }
 
         /**
-         * Destroy the component and its children.
+         * Destroy the component and its subtree.
          */
         destroy(): void {
-            this.destroyed.emit();
-            this.destroyed.disconnect();
             this._detachElement();
-            this._destroyChildren();
-            this._deparent();
             this._element = null;
-        }
-
-        /**
-         * Returns the parent Component of this component.
-         */
-        parent(): Component {
-            return this._parent;
-        }
-
-        /**
-         * Returns the array of child Components of this component.
-         */
-        children(): Component[] {
-            var children = this._children;
-            if (children) {
-                return children.slice();
-            }
-            return [];
-        }
-
-        /**
-         * Unparent the Component and detach its element from the DOM.
-         *
-         */
-        detach(): void {
-            this._detachElement();
-            this._deparent();
-        }
-
-        /**
-         * Append children to the end of this component.
-         *
-         * If a component is already a child, it will be moved to the
-         * end of the child array. The children *must* be unique.
-         *
-         * @param [...] The child Components to append.
-         */
-        append(...children: Component[]): void {
-            var fragment = this._prepareChildren(children);
-            var current = this._children || [];
-            this._children = current.concat(children);
-            this._element.appendChild(fragment);
-        }
-
-        /**
-         * Prepend children to the beginning of this component.
-         *
-         * If a component is already a child, it will be moved to the
-         * beginning of the child array. The children *must* be unique.
-         *
-         * @param [...] The child Components to prepend.
-         */
-        prepend(...children: Component[]): void {
-            var fragment = this._prepareChildren(children);
-            var current = this._children || [];
-            this._children = children.concat(current);
-            var element = this._element;
-            element.insertBefore(fragment, element.firstChild);
-        }
-
-        /**
-         * Insert children before the given child.
-         *
-         * If a component is already a child, it will be moved to the
-         * new location in the child array. The before child *must* be
-         * a current child. The children *must* be unique.
-         *
-         * @param before The child marking the insert location.
-         * @param [...] The child Components to insert.
-         */
-        insertBefore(before: Component, ...children: Component[]): void {
-            if (before._parent !== this) {
-                throw Error("'before' is not a child of this component.");
-            }
-            var fragment = this._prepareChildren(children);
-            var current = this._children || [];
-            var index = current.indexOf(before);
-            if (index === -1) {
-                this._children = current.concat(children);
-                this._element.appendChild(fragment);
-            } else {
-                var leading = current.slice(0, index);
-                var trailing = current.slice(index);
-                this._children = leading.concat(children, trailing);
-                this._element.insertBefore(fragment, before._element);
-            }
+            super.destroy();
         }
 
         /**
@@ -237,23 +145,14 @@ module porcelain {
         }
 
         /**
-         * Returns the cached geometry data for the object.
+         * Create the underlying element for the component.
          *
-         * This is intended for internal use by the framework. It is
-         * subject to change without notice and should not be used
-         * directly by user code.
+         * The default implementation creates a div.
+         *
+         * @protected.
          */
-        cachedGeometry(): IGeometryCache {
-            var cache = this._geometryCache;
-            if (!cache) {
-                cache = this._geometryCache = {
-                    rect: null,
-                    sizeHint: null,
-                    minimumSize: null,
-                    maximumSize: null,
-                };
-            }
-            return cache;
+        createElement(): HTMLElement {
+            return document.createElement("div");
         }
 
         /**
@@ -273,34 +172,65 @@ module porcelain {
         }
 
         /**
-         * Create the underlying element for the component.
+         * Returns the cached geometry data for the object.
          *
-         * The default implementation creates a div.
+         * This is intended for internal use by the framework. It is
+         * subject to change without notice and should not be used
+         * directly by user code.
          *
-         * @protected.
+         * @internal
          */
-        createElement(): HTMLElement {
-            return document.createElement("div");
-        }
-
-        /**
-         * A helper method for preparing children to be inserted.
-         *
-         * @private
-         */
-        private _prepareChildren(children: Component[]): DocumentFragment {
-            var fragment = document.createDocumentFragment();
-            for (var i = 0, n = children.length; i < n; ++i) {
-                var child = children[i];
-                child._deparent();
-                child._parent = this;
-                fragment.appendChild(child._element);
+        cachedGeometry(): IGeometryCache {
+            var cache = this._geometryCache;
+            if (!cache) {
+                cache = this._geometryCache = {
+                    rect: null,
+                    sizeHint: null,
+                    minimumSize: null,
+                    maximumSize: null,
+                };
             }
-            return fragment;
+            return cache;
         }
 
         /**
-         * A helper method to detach the DOM element.
+         * A method invoked when a child is added to the node.
+         *
+         * This must *never* be called directly by user code. The
+         * default implementation is a no-op.
+         *
+         * @param node The child node which was added.
+         *
+         * @protected
+         */
+        onChildAdded(node: Node): void { }
+
+        /**
+         * A method invoked when a child is removed from the node.
+         *
+         * This must *never* be called directly by user code. The
+         * default implementation is a no-op.
+         *
+         * @param node The child node which was removed.
+         *
+         * @protected
+         */
+        onChildRemoved(node: Node): void { }
+
+        /**
+         * A method invoked when a child is moved among its siblings.
+         *
+         * This must *never* be called directly by user code. The
+         * default implementation is a no-op.
+         *
+         * @param child The child component which was removed.
+         *
+         * @protected
+         */
+        onChildMoved(node: Node): void { }
+
+        /**
+         * The internal element detach method.
          *
          * @private
          */
@@ -312,43 +242,8 @@ module porcelain {
             }
         }
 
-        /**
-         * A helper method for destroying the component children.
-         *
-         * @private
-         */
-        private _destroyChildren(): void {
-            var children = this._children;
-            if (!children) {
-                return;
-            }
-            this._children = null;
-            for (var i = 0, n = children.length; i < n; ++i) {
-                children[i].destroy();
-            }
-        }
-
-        /**
-         * A helper method for de-parenting the component.
-         *
-         * @private
-         */
-        private _deparent(): void {
-            var parent = this._parent;
-            if (!parent) {
-                return;
-            }
-            this._parent = null;
-            var siblings = parent._children;
-            if (!siblings) {
-                return;
-            }
-            _.pull(siblings, this);
-        }
-
         private _element: HTMLElement;
-        private _parent: Component = null;
-        private _children: Component[] = null;
+        private _rendered: boolean = false;
         private _geometryCache: IGeometryCache = null;
     }
 
