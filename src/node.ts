bfeb54a58a861fd4f70e7fc16b6619c1bee2e6cc
S. Chris Colbert <sccolbert@gmail.com>
2014-04-18 14:13:55 -0400

    cleanup

--- src/node.ts
+++ src/node.ts
@@ -10,9 +10,8 @@ module porcelain {
     /**
      * A class which represents a node in a tree.
      *
-     * This class servers as the foundational tree structure for
-     * porcelain components. It will not normally be instantiated
-     * directly.
+     * This class provides the base tree structure for porcelain
+     * components. It will not normally be instantiated directly.
      *
      * @class
      */
@@ -25,21 +24,17 @@ module porcelain {
          * Destroy this node and the nodes of its subtree.
          */
         destroy(): void {
-            var next: Node;
-            var child = this._firstChild;
-            this._firstChild = null;
+            var children = this.children();
             this._lastChild = null;
-            while (child) {
-                next = child._nextSibling;
+            this._firstChild = null;
+            for (var i = 0, n = children.length; i < n; ++i) {
+                var child = children[i];
                 child._parent = null;
-                child._previousSibling = null;
                 child._nextSibling = null;
+                child._previousSibling = null;
                 child.destroy();
-                child = next;
             }
-            if (this._parent) {
-                this.remove();
-            }
+            this.remove();
         }
 
         /**
@@ -78,77 +73,16 @@ module porcelain {
         }
 
         /**
-         * Append a child node to the end of the child nodes.
-         *
-         * If the node is already a child, it will be moved.
-         *
-         * @param node The node to append to the child nodes.
+         * Returns an array of child nodes of this node.
          */
-        appendChild(node: Node): void {
-            if (this._lastChild === node) {
-                return;
+        children(): Node {
+            var nodes: Node[] = [];
+            var child = this._firstChild;
+            while (child) {
+                nodes.push(child);
+                child = child._nextSibling;
             }
-            if (node._parent === this) {
-                node._removeInternal();
-                this._appendInternal(node);
-                this.onChildMoved(node);
-            } else {
-                node.remove();
-                this._appendInternal(node);
-                this.onChildAdded(node);
-            }
-        }
-
-        /**
-         * Prepend a child node to the front of the child nodes.
-         *
-         * If the node is already a child, it will be moved.
-         *
-         * @param node The node to prepend to the child nodes.
-         */
-        prependChild(node: Node): void {
-            if (this._firstChild === node) {
-                return;
-            }
-            if (node._parent === this) {
-                node._removeInternal();
-                this._prependInternal(node);
-                this.onChildMoved(node);
-            } else {
-                node.remove();
-                this._prependInternal(node);
-                this.onChildAdded(node);
-            }
-        }
-
-        /**
-         * Insert a child node before the given refence node.
-         *
-         * If the node is already a child, it will be moved.
-         * The reference node *must* be a current child.
-         *
-         * @param node The child node to insert.
-         * @param reference The child before which 'node' is inserted.
-         */
-        insertChild(node: Node, reference: Node): void {
-            if (reference._parent !== this) {
-                throw new Error("reference node is not a child node");
-            }
-            if (node === reference) {
-                return;
-            }
-            if (reference._previousSibling === node) {
-                return;
-            }
-            if (node._parent === this) {
-                node._removeInternal();
-                this._insertInternal(node, reference);
-                this.onChildMoved(node);
-            } else {
-                node.remove();
-                this._insertInternal(node, reference);
-                this.onChildAdded(node);
-            }
+            return nodes;
         }
 
         /**
@@ -158,9 +92,94 @@ module porcelain {
          */
         remove(): void {
             var parent = this._parent;
-            if (parent) {
-                this._removeInternal();
-                parent.onChildRemoved(this);
+            if (!parent) {
+                return;
+            }
+            this._remove();
+            parent.onChildRemoved(this);
+        }
+
+        /**
+         * Append child nodes to this node.
+         *
+         * If a node is already a child, it will be moved.
+         *
+         * @param [...] The nodes to append to the child nodes.
+         */
+        append(...nodes: Node[]): void;
+        append(): void {
+            for (var i = 0, n = arguments.length; i < n; ++i) {
+                var node: Node = arguments[i];
+                if (node === this._lastChild) {
+                    continue;
+                }
+                if (node._parent === this) {
+                    node._remove();
+                    this._append(node);
+                    this.onChildMoved(node);
+                } else {
+                    node.remove();
+                    this._append(node);
+                    this.onChildAdded(node);
+                }
+            }
+        }
+
+        /**
+         * Prepend child nodes to this node.
+         *
+         * If a node is already a child, it will be moved.
+         *
+         * @param [...] The nodes to prepend to the child nodes.
+         */
+        prepend(...nodes: Node[]): void;
+        prepend(): void {
+            for (var i = arguments.length - 1, n = 0; i >= n; --i) {
+                var node: Node = arguments[i];
+                if (node === this._firstChild) {
+                    continue;
+                }
+                if (node._parent === this) {
+                    node._remove();
+                    this._prepend(node);
+                    this.onChildMoved(node);
+                } else {
+                    node.remove();
+                    this._prepend(node);
+                    this.onChildAdded(node);
+                }
+            }
+        }
+
+        /**
+         * Insert child nodes before the given refence node.
+         *
+         * If a node is already a child, it will be moved. The given
+         * reference node *must* be a current child.
+         *
+         * @param before The child before which nodes are inserted.
+         * @param [...] The child nodes to insert.
+         */
+        insert(before: Node, ...nodes: Node[]): void;
+        insert(): void {
+            var before: Node = arguments[0];
+            if (before._parent !== this) {
+                throw new Error("'before' is not a child of this node");
+            }
+            for (var i = 1, n = arguments.length; i < n; ++i) {
+                var node: Node = arguments[i];
+                if (node === before || node === before._previousSibling) {
+                    continue;
+                }
+                if (node._parent === this) {
+                    node._remove();
+                    this._insert(before, node);
+                    this.onChildMoved(node);
+                } else {
+                    node.remove();
+                    this._insert(before, node);
+                    this.onChildAdded(node);
+                }
             }
         }
 
@@ -203,58 +222,59 @@ module porcelain {
         /**
          * The internal node append method.
          *
-         * The node being appending *must not* have a parent.
+         * The node *must not* have a parent.
          *
          * @private
          */
-        private _appendInternal(node: Node): void {
-            var last = this._lastChild;
+        private _append(node: Node): void {
+            var oldLast = this._lastChild;
             this._lastChild = node;
             node._parent = this;
-            if (!last) {
+            if (!oldLast) {
                 this._firstChild = node;
             } else {
-                last._nextSibling = node;
-                node._previousSibling = last;
+                oldLast._nextSibling = node;
+                node._previousSibling = oldLast;
             }
         }
 
         /**
          * The internal node prepend method.
          *
-         * The node being prepending *must not* have a parent.
+         * The node *must not* have a parent.
          *
          * @private
          */
-        private _prependInternal(node: Node): void {
-            var first = this._firstChild;
+        private _prepend(node: Node): void {
+            var oldFirst = this._firstChild;
             this._firstChild = node;
             node._parent = this;
-            if (!first) {
+            if (!oldFirst) {
                 this._lastChild = node;
             } else {
-                first._previousSibling = node;
-                node._nextSibling = first;
+                oldFirst._previousSibling = node;
+                node._nextSibling = oldFirst;
             }
         }
 
         /**
          * The internal node insert method.
          *
-         * The node being inserted *must not* have a parent.
-         * The reference node *must* be a current child.
-         * The node *must not* be the reference node.
+         * The node *must not* have a parent and *must not* be the
+         * same node as 'before'.
          *
          * @private
          */
-        private _insertInternal(node: Node, reference: Node): void {
-            node._parent = this;
-            node._previousSibling = reference._previousSibling;
-            reference._previousSibling = node;
-            node._nextSibling = reference;
-            if (this._firstChild === reference) {
+        private _insert(before: Node, node: Node): void {
+            if (this._firstChild === before) {
                 this._firstChild = node;
+            } else {
+                node._previousSibling = before._previousSibling;
+                node._previousSibling._nextSibling = node;
             }
+            node._parent = this;
+            node._nextSibling = before;
+            before._previousSibling = node;
         }
 
         /**
@@ -264,13 +284,13 @@ module porcelain {
          *
          * @private
          */
-        private _removeInternal(): void {
+        private _remove(): void {
             var parent = this._parent;
-            var prev = this._previousSibling;
             var next = this._nextSibling;
+            var prev = this._previousSibling;
             this._parent = null;
-            this._previousSibling = null;
             this._nextSibling = null;
+            this._previousSibling = null;
             if (parent._firstChild === this) {
                 parent._firstChild = next;
             }
@@ -286,10 +306,10 @@ module porcelain {
         }
 
         private _parent: Node = null;
-        private _firstChild: Node = null;
         private _lastChild: Node = null;
-        private _previousSibling: Node = null;
+        private _firstChild: Node = null;
         private _nextSibling: Node = null;
+        private _previousSibling: Node = null;
     }
 
 }
